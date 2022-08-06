772baab73774cc95eb4f81f3da651f50d72f93bc
Alon <alonshtrauss@gmail.com>
2022-08-06 15:17:33 +0300

    8th version - added authorization

--- main.py
+++ main.py
@@ -26,33 +26,49 @@ name = ''
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        # validating that a valid name for the from and to was entered
+        try:
+            city_from = flight_search.get_code(request.form["from"])
+            city_to = flight_search.get_code(request.form["to"])
+        except:
+            flash("Please enter a valid city")
+            return redirect(url_for("home"))
+
+        input_info = {
+            "from_d": city_from,
+            "to_d": city_to,
+            "earliest_date": request.form["earliest_date"],
+            "latest_date": request.form["latest_date"],
+            "min_nights": request.form["min_nights"],
+            "max_nights": request.form["max_nights"],
+            "type_of_trip": request.form["drop-down-menu"],
+            "num_people": request.form["num_people"],
+            "adults": request.form["adults"],
+            "children": request.form["children"],
+            "infants": request.form["infants"],
+            "num_baggage": request.form["num_baggage"],
+            "hold_bag": request.form["hold_bag"],
+            "hand_bag": request.form["hand_bag"],
+
+        }
+        # if the user tries to select too many bags, this will stop him
+        try:
+            results_data = flight_search.search(input_info)
+        except:
+            flash("Too many bags - please select up to 1 hand bag and 2 hold bags per person.")
+            return redirect(url_for("home"))
+
+        # if the dates are not valid then the program will fail in the line below,
+        # so the except prevents it
+        try:
+            results = formatter.data_formatter(results_data["data"])
+        except:
+            flash("Please enter valid dates")
+            return redirect(url_for("home"))
+        return render_template("results.html", info=input_info, search_result=results, is_logged_in=is_logged_in,
+                               name=name)
     return render_template("home_page.html", is_logged_in=is_logged_in, name=name)
-
-
-@app.route("/results", methods=["GET", "POST"])
-def results_page():
-    input_info = {
-        "from_d": flight_search.get_code(request.form["from"]),
-        "to_d": flight_search.get_code(request.form["to"]),
-        "earliest_date": request.form["earliest_date"],
-        "latest_date": request.form["latest_date"],
-        "min_nights": request.form["min_nights"],
-        "max_nights": request.form["max_nights"],
-        "type_of_trip": request.form["drop-down-menu"],
-        "num_people": request.form["num_people"],
-        "adults": request.form["adults"],
-        "children": request.form["children"],
-        "infants": request.form["infants"],
-        "num_baggage": request.form["num_baggage"],
-        "hold_bag": request.form["hold_bag"],
-        "hand_bag": request.form["hand_bag"],
-
-    }
-    results_data = flight_search.search(input_info)
-    results = formatter.data_formatter(results_data["data"])
-    global is_logged_in
-    global name
-    return render_template("results.html", info=input_info, search_result=results, is_logged_in=is_logged_in, name=name)
 
 
 @app.route("/login", methods=["GET", "POST"])
